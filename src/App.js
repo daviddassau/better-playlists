2b936dfc3503a34eaf183f7ee53b46b1ff5b8949
David T. Dassau <davidtdassau@gmail.com>
2019-07-15 11:58:54 -0500

    filter also applies to all the datas

--- src/App.js
+++ src/App.js
@@ -1,139 +1,3 @@
-// import React, { Component } from 'react';
-// import './App.css';
-
-// let defaultStyle = {
-//   color: '#fff'
-// };
-// let fakeServerData = {
-//   user: {
-//     name: 'David'
-//   }
-// };
-
-// class Aggregate extends Component {
-//   render() {
-//     return(
-//       <div style={{...defaultStyle, width: "40%", display: "inline-block"}}>
-//         <h2>Number Text</h2>
-//       </div>
-//     );
-//   }
-// }
-
-// class Filter extends Component {
-//   render() {    
-//     return (
-//       <div style={defaultStyle}>
-//         <img />
-//         <input type="text"/>
-//       </div>
-//     );
-//   }
-// }
-
-// class Playlist extends Component {
-//   render() {
-//     return (
-//       <div style={{...defaultStyle, display: "inline-block", width: "20%"}}>
-//         <img/>
-//         <h3>Playlist Name</h3>
-//         <ul>
-//           <li>Song 1</li>
-//           <li>Song 2</li>
-//           <li>Song 3</li>
-//         </ul>
-//       </div>
-//     );
-//   }
-// }
-
-// function App() {
-//   // constructor() {
-//   //   super();
-//   //   this.state = {serverData: {}}
-//   // }
-//   // componentDidMount() {
-//   //   this.setState({serverData: fakeServerData});
-//   // }
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <h1 style={{...defaultStyle, 'font-size':'54px'}}>{fakeServerData.user.name}'s Playlist</h1>
-//         <Aggregate/>
-//         <Aggregate/>
-//         <Filter/>
-//         <Playlist/>
-//         <Playlist/>
-//         <Playlist/>
-//         <Playlist/>
-//       </header>
-//     </div>
-//   );
-// }
-
-// // class App extends Component() {
-// //   constructor(props) {
-// //     super(props);
-// //     this.state = {serverData: {}}
-// //   }
-// //   componentDidMount() {
-// //     this.setState({serverData: fakeServerData});
-// //   }
-// //   render() {
-// //     return (
-// //       <div className="App">
-// //       <header className="App-header">
-// //         <h1 style={{...defaultStyle, 'font-size':'54px'}}>
-// //           {this.state.serverData.user && this.state.serverData.user.name}'s Playlist
-// //         </h1>
-// //         <Aggregate/>
-// //         <Aggregate/>
-// //         <Filter/>
-// //         <Playlist/>
-// //         <Playlist/>
-// //         <Playlist/>
-// //         <Playlist/>
-// //       </header>
-// //     </div>
-// //     );
-// //   }
-// // }
-
-// // class App extends React.Component() {
-// //   constructor(props) {
-// //     super(props);
-// //     this.state = {serverData: {}}
-// //   }
-// //   componentDidMount() {
-// //     this.setState({serverData: fakeServerData});
-// //   }
-// //   render() {
-// //     return (
-// //       <div className="App">
-// //       <header className="App-header">
-// //         <h1 style={{...defaultStyle, 'font-size':'54px'}}>
-// //           {this.state.serverData.user && this.state.serverData.user.name}'s Playlist
-// //         </h1>
-// //         <Aggregate/>
-// //         <Aggregate/>
-// //         <Filter/>
-// //         <Playlist/>
-// //         <Playlist/>
-// //         <Playlist/>
-// //         <Playlist/>
-// //       </header>
-// //     </div>
-// //     );
-// //   }
-// // }
-
-// export default App;
-
-
-
-
-
-
 import React, { Component } from 'react';
 import './App.css';
 
@@ -249,7 +113,11 @@ class App extends Component {
     }, 1000);
   }
   render() {
-
+    let playlistToRender = this.state.serverData.user ? this.state.serverData.user.playlists
+      .filter(playlist => 
+        playlist.name.toLowerCase().includes(
+          this.state.filterString.toLowerCase())
+    ) : []
     return (
       <div className="App">
         {this.state.serverData.user ?
@@ -257,15 +125,12 @@ class App extends Component {
           <h1 style={{...defaultStyle, 'font-size': '54px'}}>
             {this.state.serverData.user.name}'s Playlists
           </h1>
-          <PlaylistCounter playlists={this.state.serverData.user.playlists}/>
-          <HoursCounter playlists={this.state.serverData.user.playlists}/>
+          <PlaylistCounter playlists={playlistToRender}/>
+          <HoursCounter playlists={playlistToRender}/>
           <Filter onTextChange={text => {
               this.setState({filterString: text})
             }}/>
-          {this.state.serverData.user.playlists.filter(playlist => 
-            playlist.name.toLowerCase().includes(
-              this.state.filterString.toLowerCase())
-          ).map(playlist => 
+          {playlistToRender.map(playlist => 
             <Playlist playlist={playlist} />
           )}
         </div> : <h1 style={defaultStyle}>Loading...</h1>
